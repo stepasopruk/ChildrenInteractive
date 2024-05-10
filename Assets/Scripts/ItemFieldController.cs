a6cad9dbe42f571af89f07164001188d1903e8e6
Djoule <61910104+stepasopruk@users.noreply.github.com>
2024-05-10 12:54:46 +0300

    Intermix item

--- Assets/Scripts/ItemFieldController.cs
+++ Assets/Scripts/ItemFieldController.cs
@@ -1,19 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class ItemFieldController : SubstitutionLettersFieldController
 {
     [SerializeField] private Item itemPrefab;
 
+    private List<Transform> _itemsPos = new();
+
     public override void Create(LetterData data)
     {
         Item item = Instantiate(itemPrefab, content);
-        item.ItemSprite = data.ItemSprite;
+        item.ItemImage.sprite = data.ItemSprite;
     }
 
     public Item CreateItem(LetterData data)
     {
         Item item = Instantiate(itemPrefab, content);
-        item.ItemSprite = data.ItemSprite;
+        item.ItemImage.sprite = data.ItemSprite;
+        _itemsPos.Add(item.transform);
         return item;
+    }
+
+    public void Intermix()
+    {
+        for (int i = 0; i < _itemsPos.Count; i++)
+        {
+            int randomIndex = UnityEngine.Random.Range(0, _itemsPos.Count);
+            _itemsPos[i].SetSiblingIndex(randomIndex);
+        }
     }
 }
